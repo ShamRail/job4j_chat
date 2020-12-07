b9d34846b5c915ead835d6906791dad6600bcf2a
Rail <rail1999g102r@mail.ru>
2020-12-07 18:43:20 +0500

    Init commit

---
+++ src/main/java/ru/job4j/chat/controller/TestController.java
@@ -0,0 +1,16 @@
+package ru.job4j.chat.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @GetMapping
+    public String echo() {
+        return "Hello";
+    }
+
+}
---
+++ src/test/java/ru/job4j/chat/ChatApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.job4j.chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
